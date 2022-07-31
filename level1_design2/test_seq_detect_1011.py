005d1c4a0ef1c133e33173e8d0caa235ade92075
Ai-deen <105343698+Ai-deen@users.noreply.github.com>
2022-07-31 12:20:04 +0530

    Update test_seq_detect_1011.py

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -40,10 +40,12 @@ async def test_seq_bug_1(dut):
     dut.inp_bit.value = 1
     await FallingEdge(dut.clk)
 
+
+    dut._log.info(f' EXPECT={1} DUT={dut.seq_seen.value}')
     assert dut.seq_seen.value == 1, f'Sequence 1011 is not detected due to error...Entered sequence is :0011011 '
 
 @cocotb.test()
-async def test_seq_bug_2(dut):
+async def test_seq_bug2(dut):
     """Test for seq detection """
 
     clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
@@ -77,4 +79,49 @@ async def test_seq_bug_2(dut):
     dut.inp_bit.value = 1
     await FallingEdge(dut.clk)
 
+    dut._log.info(f' EXPECT={1} DUT={dut.seq_seen.value}')
     assert dut.seq_seen.value == 1, f'Sequence 1011 is not detected due to error...Entered sequence is :00101011'
+
+
+
+@cocotb.test()
+async def test_seq_bug3(dut):
+    """Test for seq detection """
+
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+
+    dut._log.info(f'Input bit: {dut.inp_bit.value}')
+    
+    # reset
+    dut.reset.value = 1
+    await FallingEdge(dut.clk)
+    dut.reset.value = 0
+    await FallingEdge(dut.clk)
+
+    cocotb.log.info('#### CTB: Develop your test here! ######')
+
+    dut.inp_bit.value = 0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 0
+    await FallingEdge(dut.clk)    
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+
+
+    dut._log.info(f' EXPECT={0} DUT={dut.seq_seen.value}')
+    assert dut.seq_seen.value == 0, f'Sequence 1011 is detected due to error...Entered sequence is :0010110111'
